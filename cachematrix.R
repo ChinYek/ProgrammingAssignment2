073a3e6d29623ed3d7a24d2114e076c0f44ee280
ChinYek <cy.yek0729@gmail.com>
2017-04-12 21:51:11 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to compute the inverse of an invertible 	
+## matrix (which has to be square matrix)
 
-## Write a short comment describing this function
+## The process is to call out the function makeCacheMatrix first(which 	
+## creates a special 'matrix' object to cache the inverse of matrix)
+ 	
+## Then, it will call out the second function cacheSolve(which compute 
+## the inverse of 'matrix' returned in makeCacheMatrix; this function 
+## will return the inverse of input matrix and cache the inverse if the 
+## input matrix is new to the function; else if the inverse has been 
+## calculated and cached before, the inverse will retrieve from cache)
+
+## makeCacheMatrix is a function which will create a special 'matrix'
+## object (a list), use to cache the inverse of input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	n <- NULL
+	setmatrix <- function(y) {
+		x <<- y
+		n <<- NULL
+	}
+	getmatrix <- function() x
+	setinverse <- function(inverse) n <<- inverse
+	getinverse <- function() n
+	list(setmatrix = setmatrix, getmatrix = getmatrix,
+	setinverse = setinverse,getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that return the inverse of matrix (input
+## of makeCacheMatrix); it plays 2 roles, if the inverse of input 
+## function has been calculated and cached before, cacheSolve will 
+## deirectly return the inverse; else if the input matrix is a new
+## comer, cachesolve will return 'getting cached data' and inverse
+## out
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	n <- x$getinverse()
+	if(!is.null(n)) {
+		message("getting cached data")
+            return(n)
+	}
+      datamatrix <- x$getmatrix()
+	n <- solve(datamatrix)
+      x$setinverse(n)
+      n
 }
